7bcf57435ea9c4c63bd3c89a4ba9f89dbdb81346
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-07-27 01:04:52 +0530

    Time: 30 ms (30.62%), Space: 19.8 MB (75.82%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int trap(vector<int>& height)
     {
-        int prefix[height.size()];
+       /* int prefix[height.size()];
         int p=height[0];
         for(int i =0;i<height.size();i++)
         {
             if (height[i]>p)
                 p=height[i];
             prefix[i]=p;
-        }
-        
-        
+        } 
         int suffix[height.size()];
         int s=height[height.size()-1];
         for(int i=height.size()-1;i>=0;i--)
@@ -24,17 +22,36 @@ public:
         for(int i=0;i<height.size();i++)
         {
             answer+=min(prefix[i],suffix[i])-height[i];
-        }
-        
-        
-        
-        
-        
+        } 
         for(int i=0;i<height.size();i++)
             cout<<prefix[i]<<" ";
         cout<<endl;
         for(int i=0;i<height.size();i++)
             cout<<suffix[i]<<" ";
         return answer;
+        */
+        int l=0;
+        int r= height.size()-1;
+        int lmax=0,rmax=0;
+        int answer=0;
+        while (l<=r)
+        {
+            if(height[l]<=height[r]){
+                if (height[l]>=lmax)lmax=height[l];
+                else answer+=lmax-height[l];
+                l++;
+            }
+            else{
+                if (height[r]>=rmax)rmax=height[r];
+                else answer+=rmax-height[r];
+                r--;
+            }
+        }
+        return answer;
+        
+        
+        
+        
+        
     }
 };
