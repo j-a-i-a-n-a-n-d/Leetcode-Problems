7becac21f0e1e277b2f804b0481a59580c272132
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-08-11 16:18:31 +0530

    Time: 17 ms (65.02%), Space: 21.7 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     void preorder(TreeNode *root,vector<int>&arr)
     {
@@ -32,5 +32,22 @@ public:
                 return false;
         }
         return true;
+    }
+};*/
+class Solution {
+public:
+    
+    bool solve(TreeNode* root, long long int min,long long int max){
+        if(!root)return true;
+        
+        if(root->val>=max || root->val<=min)return false;
+        
+        return solve(root->left,min,root->val) and solve(root->right,root->val,max);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        if(!root)return true;
+        
+        return solve(root,-1e18,1e18);
     }
 };
