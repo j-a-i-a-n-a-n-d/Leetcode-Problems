5e484c4c9bba9514df8512d259d1d52bf61b57b6
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-08-14 21:23:51 +0530

    Time: 30 ms (6.92%), Space: 6 MB (39.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
+    int m[32];
 public:
+    Solution()
+    {
+        memset(m,-1,sizeof(m));
+    }
     int fib(int n)
     {
-        if (n==1)return 1;
-        else if (n==0) return 0;
+        if (n==1)return m[n]=1;
+        else if (n==0) return m[n]=0;
         else 
-            return fib(n-1)+fib(n-2);
+            return m[n]=fib(n-1)+fib(n-2);
     }
 };
