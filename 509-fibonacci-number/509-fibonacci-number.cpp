97511bd571caec9f9828daa28c16fc600522c4ae
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-08-14 21:29:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,4 +1,20 @@
 class Solution {
+public:
+    int fib(int n) {
+        int dp[n+1];
+         dp[0]=0;
+        if (n==0)return 0;
+        if (n==1)return 1;
+        dp[1]=1;
+        for (int i=2;i<n+1;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+/*class Solution {
     int m[32];
 public:
     Solution()
@@ -12,4 +28,4 @@ public:
         else 
             return m[n]=fib(n-1)+fib(n-2);
     }
-};
+};*/
