07222fc2e19b590cfcea0fe10d38375430a96d03
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-08-18 20:19:02 +0530

    Time: 15 ms (13.98%), Space: 10.3 MB (29.79%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -10,36 +10,64 @@
  */
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) 
-    {
-        if (!head)return NULL;
-        ListNode *left =  new ListNode();
-        ListNode *right = new ListNode();
-        ListNode* leftStart= NULL;
-        ListNode* rightStart = NULL;
-        bool leftExist=false;
-        bool rightExist=false;
-        while(head)
-        {
-            if (head->val <x)
-            {
-                if(!leftExist){leftStart=head;leftExist=true;}
-                left->next=head;
-                left=head;
-            }
-            else
-            {
-                if(!rightExist){rightStart=head;rightExist=true;}
-                right->next=head;
-                right=head;
-            }
-            head=head->next;
-        }
-        right->next=NULL; //MOST IMPORTANT LINE figured after an hour :(
-        if (!rightStart)return leftStart;
-        if (!leftStart)return rightStart;
-        left->next=rightStart;
-        return leftStart;
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *left = new ListNode(0);
+        ListNode *right = new ListNode(0);
         
+        ListNode *leftTail = left;
+        ListNode *rightTail = right;
+        
+        while(head != NULL){
+            if(head->val < x){
+                leftTail->next = head;
+                leftTail = leftTail->next;
+            }
+            else{
+                rightTail->next = head;
+                rightTail = rightTail->next;
+            }
+            head = head->next;
+        }
+        
+        leftTail->next = right->next;
+        rightTail->next = NULL;
+        
+        return left->next;
     }
 };
+
+//class Solution {
+//public:
+//    ListNode* partition(ListNode* head, int x) 
+//    {
+//        if (!head)return NULL;
+//        ListNode *left =  new ListNode();
+//        ListNode *right = new ListNode();
+//        ListNode* leftStart= NULL;
+//        ListNode* rightStart = NULL;
+//        bool leftExist=false;
+//        bool rightExist=false;
+//        while(head)
+//        {
+//            if (head->val <x)
+//            {
+//                if(!leftExist){leftStart=head;leftExist=true;}
+//                left->next=head;
+//                left=head;
+//            }
+//            else
+//            {
+//                if(!rightExist){rightStart=head;rightExist=true;}
+//                right->next=head;
+//                right=head;
+//            }
+//            head=head->next;
+//        }
+//        right->next=NULL; //MOST IMPORTANT LINE figured after an hour :(
+//        if (!rightStart)return leftStart;
+//        if (!leftStart)return rightStart;
+//        left->next=rightStart;
+//        return leftStart;
+//        
+//    }
+//};//
