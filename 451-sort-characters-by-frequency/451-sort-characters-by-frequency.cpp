7c9c163a67a3f2066371fb4994ea6e365d6c9384
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-08-22 00:59:27 +0530

    Time: 4 ms (99.60%), Space: 8.8 MB (15.18%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string frequencySort(string s) 
+    {
+        priority_queue<pair<int,char>>pq;
+        vector<int> mp(126,0);
+        for(int i=0;i<s.size();i++)
+                mp[s[i]]++;
+        for(int it=0;it<126;it++)
+        {
+            if(mp[it]!=0)
+                 pq.push(make_pair(mp[it],it));
+        }
+        string sol="";
+        while (pq.size()!=0)
+        {
+            pair<int,char>top=pq.top();
+                sol+=string(top.first, top.second);
+            pq.pop();
+        }
+        return sol;
+    }
+        
+};
+//string frequencySort(string s) 
+//   
+//    
+//    priority_queue<pair<int,char>>pq;
+//        veto
+//        for(int i=0;i<s.size();i++)
+//        {
+//            if (mp.find(s[i])!=mp.end())
+//                mp[s[i]]++;
+//            else
+//                mp[s[i]]=1;
+//        }
+//        for(auto it : mp)
+//        {
+//            pq.push(make_pair(it.second,it.first));
+//        }
+//        string sol="";
+//        while (pq.size()!=0)
+//        {
+//            pair<int,char>top=pq.top();
+//            for(int i=1;i<=top.first;i++)
+//                sol=sol+top.second;
+//            pq.pop();
+//        }
+//        return sol;
+//    }//
