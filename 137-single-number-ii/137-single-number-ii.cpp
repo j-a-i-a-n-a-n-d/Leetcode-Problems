56425af0c71fd871b53d74c43287120c9216a735
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-09-16 23:47:13 +0530

    Time: 18 ms (31.82%), Space: 9.4 MB (99.08%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) 
+    int singleNumber(vector<int>& nums)
     {
-        unordered_map<int,int> map1;
-        for(int i=0;i<nums.size();i++){
-            map1[nums[i]]++;
-        }
-        for(auto it : map1)
+        int ones=0;
+        int twos=0;
+        //bool s=false;
+        //sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
         {
-            if (it.second==1)
-                return it.first;
+                ones=(ones^nums[i])&(~twos);
+                twos=(twos^nums[i])&(~ones);
         }
-        return 0;
+        return ones;
     }
 };
+//class Solution {
+//public:
+//    int singleNumber(vector<int>& nums) 
+//    {
+//        unordered_map<int,int> map1;
+//        for(int i=0;i<nums.size();i++){
+//            map1[nums[i]]++;
+//        }
+//        for(auto it : map1)
+//        {
+//            if (it.second==1)
+//                return it.first;
+//        }
+//        return 0;
+//    }
+//};
