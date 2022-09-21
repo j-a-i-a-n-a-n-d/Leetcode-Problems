c4867f928ce33a3364cc167551624cbf1f3f10c5
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-09-21 16:49:08 +0530

    Time: 496 ms (13.10%), Space: 358.8 MB (13.21%) - LeetHub

--- 398-random-pick-index/398-random-pick-index.cpp
+++ 398-random-pick-index/398-random-pick-index.cpp
@@ -1,24 +1,39 @@
-class Solution {
-public:
-    vector<int> nums;
-    Solution(vector<int>& nums) 
-    {
-        this->nums=nums;
-    }
-    int pick(int target)
-    {
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++)
-        {
-            if (nums[i]==target)ans.push_back(i);
-        }
-        //return ans.size()!=0?ans[rand()%ans.size()]:-1;
-        return ans[rand()%ans.size()];
-    }
-};
+//class Solution {
+//public:
+//    vector<int> nums;
+//    Solution(vector<int>& nums) 
+//    {
+//        this->nums=nums;
+//    }
+//    int pick(int target)
+//    {
+//        vector<int> ans;
+//        for(int i=0;i<nums.size();i++)
+//        {
+//            if (nums[i]==target)ans.push_back(i);
+//        }
+//        //return ans.size()!=0?ans[rand()%ans.size()]:-1;
+//        return ans[rand()%ans.size()];
+//    }
+//};
 
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution* obj = new Solution(nums);
  * int param_1 = obj->pick(target);
  */
+class Solution {
+private:
+    unordered_map<int, vector<int>> m;
+public:
+    Solution(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++)
+            m[nums[i]].push_back(i);
+    }
+    
+    int pick(int target) {
+        auto picked = m[target];
+        int n = picked.size();
+        return picked[rand()%n];
+    }
+};
