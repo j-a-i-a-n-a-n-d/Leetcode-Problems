56badf7ddabd6f8d105d69324a851cc7af0f7051
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-11-10 13:01:47 +0530

    Time: 45 ms (63.41%), Space: 10.9 MB (66.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    string removeDuplicates(string s)
+    string removeDuplicates(string s) 
     {
-        int n = s.size();
-        stack<char> stock;
-        for(int i=0;i<n;i++)
+        string res="";
+        for(auto c : s)
         {
-            if(!stock.empty() && stock.top()==s[i])
-                stock.pop();
+            if (res.size()!=0 && res.back()==c)
+                res.pop_back();
             else
-                stock.push(s[i]);
+                res+=c;
         }
-        string sol = "";
-        while(!stock.empty())
-        {
-            sol= string(stock.top()+sol);
-            stock.pop();
-        }
-        return sol;
+        return res;
     }
 };
+
+//class Solution {
+//public:
+//    string removeDuplicates(string s)
+//    {
+//        int n = s.size();
+//        stack<char> stock;
+//        for(int i=0;i<n;i++)
+//        {
+//            if(!stock.empty() && stock.top()==s[i])
+//                stock.pop();
+//            else
+//                stock.push(s[i]);
+//        }
+//        string sol = "";
+//        while(!stock.empty())
+//        {
+//            sol= string(stock.top()+sol);
+//            stock.pop();
+//        }
+//        return sol;
+//    }
+//};
