f61736cab27c2b83d3511357f78e0353c25c74dc
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-11-24 08:14:22 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,20 @@
-class Solution {
-    vector<int>mem;
-public:
-    int okBhai(vector<int>&nums,int n)
-    {
-        if(n<1)return 0;
-        if(mem[n-1]!=-1)
-            return mem[n-1];
-        else
-            return mem[n-1]=max(nums[n-1]+okBhai(nums,n-2),okBhai(nums,n-1));
-    }
-    int rob(vector<int>& nums) {
-        mem.assign(nums.size(),-1);
-        return okBhai(nums,nums.size());
-    }
-};
-
-/*
+//class Solution {
+//    vector<int>mem;
+//public:
+//    int okBhai(vector<int>&nums,int n)
+//    {
+//        if(n<1)return 0;
+//        if(mem[n-1]!=-1)
+//            return mem[n-1];
+//        else
+//            return mem[n-1]=max(nums[n-1]+okBhai(nums,n-2),okBhai(nums,n-1));
+//    }
+//    int rob(vector<int>& nums) {
+//        mem.assign(nums.size(),-1);
+//        return okBhai(nums,nums.size());
+//    }
+//};
+//
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -32,4 +31,3 @@ public:
         return curr_ans;
     }
 }; 
-*/
