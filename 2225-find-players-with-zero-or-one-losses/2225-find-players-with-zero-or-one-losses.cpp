94a152262625902f0d068267fbfec66b6b80209b
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-11-28 12:03:03 +0530

    Time: 1112 ms (72.13%), Space: 159.6 MB (96.57%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -9,25 +9,20 @@ public:
         for(int i =0;i<m;i++)
             mp[matches[i][1]]++;
         
-        vector<int> oneLosers;
-        vector<int> winners;
-        
+        vector<vector<int>> sol(2);
         for(auto it : mp)
-            if(it.second==1)oneLosers.push_back(it.first);
+            if(it.second==1)sol[1].push_back(it.first);
         
         
         for(int i =0;i<m;i++)
             if(mp.find(matches[i][0])==mp.end() && s.find(matches[i][0])==s.end())
             {
                 s.insert(matches[i][0]);
-                winners.push_back(matches[i][0]);
+                sol[0].push_back(matches[i][0]);
             }
         
-        sort(winners.begin(),winners.end());
-        sort(oneLosers.begin(),oneLosers.end());
-        vector<vector<int>> sol;
-        sol.push_back(winners);
-        sol.push_back(oneLosers);
+        sort(sol[0].begin(),sol[0].end());
+        sort(sol[1].begin(),sol[1].end());
         return sol;
     }
 };
