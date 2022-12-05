6a221c8da484d71a5343701551e42a4ae49f03d1
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-12-05 09:26:54 +0530

    Time: 408 ms (45.38%), Space: 92.6 MB (28.71%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -1,25 +1,45 @@
-#define ll long long
+#define ll long long int 
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<ll int>ps(nums.size());
-        vector<ll int>ss(nums.size());
-        
+        vector<ll> ps(n);
         ps[0]=nums[0];
-        for(int i =1;i<n;i++)ps[i]=nums[i]+ps[i-1];
-        
-        ss[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--)ss[i]=nums[i]+ss[i+1];
-        
-        //for(int i =0;i<n;i++)cout<<ps[i]<<" ";
-        //cout<<endl;
-        //for(int i=0;i<n;i++)cout<<ss[i]<<" ";
+        for(int i=1;i<n;i++)ps[i]=nums[i]+ps[i-1];
         
         int sol=0;
-        
-        for(int i=0;i<n-1;i++)if (ps[i]>=ss[i+1])sol++;
+        ll totalSum=ps[n-1];
+        for(int i =0;i<n-1;i++)
+        {
+            ll x = totalSum - ps[i];
+            if (ps[i]>=x)sol++;
+        }
         return sol;
     }
 };
+//#define ll long long
+//class Solution {
+//public:
+//    int waysToSplitArray(vector<int>& nums) 
+//    {
+//        int n = nums.size();
+//        vector<ll int>ps(nums.size());
+//        vector<ll int>ss(nums.size());
+//        
+//        ps[0]=nums[0];
+//        for(int i =1;i<n;i++)ps[i]=nums[i]+ps[i-1];
+//        
+//        ss[n-1]=nums[n-1];
+//        for(int i=n-2;i>=0;i--)ss[i]=nums[i]+ss[i+1];
+//        
+//        for(int i =0;i<n;i++)cout<<ps[i]<<" ";
+//        cout<<endl;
+//        for(int i=0;i<n;i++)cout<<ss[i]<<" ";
+//        
+//        int sol=0;
+//        
+//        for(int i=0;i<n-1;i++)if (ps[i]>=ss[i+1])sol++;
+//        return sol;
+//    }
+//};//
