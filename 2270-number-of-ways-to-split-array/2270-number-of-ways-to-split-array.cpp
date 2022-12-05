5500c992099cd9227dd2505f853ddca57298a05d
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2022-12-05 09:35:50 +0530

    Time: 549 ms (6.28%), Space: 85.5 MB (64.85%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -1,23 +1,47 @@
-#define ll long int 
 class Solution {
 public:
-    int waysToSplitArray(vector<int>& nums) 
-    {
-        int n = nums.size();
-        vector<ll> ps(n);
-        ps[0]=nums[0];
-        for(int i=1;i<n;i++)ps[i]=nums[i]+ps[i-1];
+    int waysToSplitArray(vector<int>& nums) {
+     
+        long long back=0ll,front=0ll;
+        for (auto& i : nums)     back += i;
         
-        int sol=0;
-        ll totalSum=ps[n-1];
-        for(int i =0;i<n-1;i++)
-        {
-            ll x = totalSum - ps[i];
-            if (ps[i]>=x)sol++;
+        int n(size(nums)), res(0);
+        for (auto i=0; i<n-1; i++) {
+            
+            front += nums[i];
+            back -= nums[i]; 
+            if (front >= back)  res++;
         }
-        return sol;
+        return res;
     }
 };
+
+
+//space O(n)
+//time O(n)
+//#define ll long int 
+//class Solution {
+//public:
+//    int waysToSplitArray(vector<int>& nums) 
+//    {
+//        int n = nums.size();
+//        vector<ll> ps(n);
+//        ps[0]=nums[0];
+//        for(int i=1;i<n;i++)ps[i]=nums[i]+ps[i-1];
+//        
+//        int sol=0;
+//        ll totalSum=ps[n-1];
+//        for(int i =0;i<n-1;i++)
+//        {
+//            ll x = totalSum - ps[i];
+//            if (ps[i]>=x)sol++;
+//        }
+//        return sol;
+//    }
+//};
+
+//space O(2n)
+//time O(n)
 //#define ll long long
 //class Solution {
 //public:
