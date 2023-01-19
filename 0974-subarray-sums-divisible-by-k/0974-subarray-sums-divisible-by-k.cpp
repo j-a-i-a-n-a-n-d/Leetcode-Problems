502403d7e4d179f74ca7aa1b0629544c573bcb2c
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2023-01-19 20:07:52 +0530

    Time: 53 ms (85.71%), Space: 30.2 MB (86.10%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        for(int i =1;i<n;i++)nums[i]+=nums[i-1];
+        
+        //keeping count of all the reminders that we encounter
+        //1st time it is rem from 2nd time it shows that s1- rem ==0 and hence we must count it
+        //:-)
+        
+        vector<int> helperArray(k,0);
+        helperArray[0]=1;
+        int sol=0;
+        int rem=0;
+        for(auto i : nums)
+        {
+            
+            rem = (i%k);
+            rem=i<0?(k+rem)%k:rem;
+            sol = sol+ helperArray[rem];
+            helperArray[rem]++;
+        }
+        return sol;
+    }
+};
+//class Solution {
+//public:
+//    int subarraysDivByK(vector<int>& nums, int k) 
+//    {
+//        int n = nums.size();
+//        vector<int>prefixSum(n,0);
+//        prefixSum[0]=nums[0];
+//        for(int i =1;i<n;i++)
+//        {
+//            prefixSum[i]=prefixSum[i-1]+nums[i];
+//        }
+//        int count=0;
+//
+//        for(auto it : prefixSum)if (it%k==0)count++;
+//        
+//        
+//        for(int i=0;i<n;i++)
+//        {
+//            for(int j=i+1;j<n;j++)
+//            {
+//                int c =abs(prefixSum[j]-prefixSum[i]);
+//                if (c%k==0)count++;
+//            }
+//        }
+//        
+//        return count;
+//    }
+//};
