e926a090211bf7c98e141456a9df8bf9c75a3956
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2023-01-20 17:49:20 +0530

    Time: 28 ms (69.91%), Space: 31.2 MB (57.77%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    int pivotIndex(vector<int>& nums)
+    int pivotIndex(vector<int>& nums) 
     {
-        int n = nums.size();
-        vector<int> ps(n,0); 
-        ps[0]=nums[0];
-        for(int i =1;i<n;i++)ps[i]=ps[i-1]+nums[i];
-        for(auto i : ps )cout<<i<<" ";
+        int leftSum=0;
+        int rightSum=0;
+        for(auto i : nums)rightSum+=i;
         
-        int result =-1;
-        ps.insert(ps.begin(),0);
-        int ppp = ps.size();
-        for(int i =1;i<ppp;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if (ps[i-1]==ps[ppp-1]-ps[i])return i-1;
+            rightSum-=nums[i];
+            if (leftSum==rightSum)return i;
+            leftSum+=nums[i];
         }
         return -1;
-        
     }
 };
+
+
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums)
+//     {
+//         int n = nums.size();
+//         vector<int> ps(n,0); 
+//         ps[0]=nums[0];
+//         for(int i =1;i<n;i++)ps[i]=ps[i-1]+nums[i];
+//         for(auto i : ps )cout<<i<<" ";
+        
+//         int result =-1;
+//         ps.insert(ps.begin(),0);
+//         int ppp = ps.size();
+//         for(int i =1;i<ppp;i++)
+//         {
+//             if (ps[i-1]==ps[ppp-1]-ps[i])return i-1;
+//         }
+//         return -1;
+        
+//     }
+// };
