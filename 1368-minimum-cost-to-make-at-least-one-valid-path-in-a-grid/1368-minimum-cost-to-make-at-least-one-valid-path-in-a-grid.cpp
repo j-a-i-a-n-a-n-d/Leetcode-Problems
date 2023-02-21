945416d0670743a5b40ad34442f75259fb83056e
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2023-02-22 01:00:15 +0530

    Time: 166 ms (28.51%), Space: 23.4 MB (30.07%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    typedef struct node
+    {
+        int get1,get2,get3;
+    }node;
+    bool isValid(int i,int j,int n,int m){
+        return i>=0 && i<n && j>=0 && j<m;
+    }
+    int minCost(vector<vector<int>>& grid) 
+    {
+        vector<vector<int>> vps= {{0,1},{0,-1},{1,0},{-1,0}};
+                                 //  0       1     2      3
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
+        deque<node> dq;
+        dq.push_front({0,0,0});
+        // i j dis
+        dis[0][0]=0;
+        
+        while(!dq.empty())
+        {
+            node temp = dq.front();
+            dq.pop_front();
+            
+            int a = temp.get1;
+            int b = temp.get2;
+            int d = temp.get3;
+            
+            int direction = grid[a][b];
+            for(int i=0;i<4;i++)
+            {
+                int ii = a+vps[i][0];
+                int jj = b+vps[i][1];
+                if (isValid(ii,jj,n,m) && direction==i+1 && (dis[a][b] + d < dis[ii][jj]))
+                {
+                    dis[ii][jj]=dis[a][b] + d;
+                    dq.push_front({ii,jj,0});
+                }
+                else if (isValid(ii,jj,n,m) && (dis[a][b] + d < dis[ii][jj]))
+                {
+                    dis[ii][jj]=dis[a][b] + d+1;
+                    dq.push_back({ii,jj,0});
+                }       
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<dis[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return dis[n-1][m-1];    
+    }
+};
