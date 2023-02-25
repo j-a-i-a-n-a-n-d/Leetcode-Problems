62a3046dcd946dbe80ff99fd9d1c6b9b556a6547
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2023-02-25 17:59:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (30.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -3,19 +3,10 @@ public:
     int minAddToMakeValid(string s)
     {
         stack<char> st;
-        
-        for(auto ch : s)
-        {
-            if(!st.empty() && st.top()=='(' && ch==')')
-            {
-                st.pop();
-            }
-            else
-            {
-                st.push(ch);
-            }
+        for(auto ch : s){
+            if(!st.empty() && st.top()=='(' && ch==')')st.pop();
+            else st.push(ch);
         }
-        int sol = st.size();
-        return sol;
+        return st.size();;
     }
 };
