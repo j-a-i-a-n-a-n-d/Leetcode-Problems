7b31c727a146c4f0a443056bce56a1febe469832
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2023-08-24 17:49:08 +0530

    Time: 2601 ms (5.02%), Space: 6.7 MB (42.61%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int dp[31][31];
+    
+    int Combination(int n,int r)
+    {
+        if (r==0 || n==r)return 1;
+        if (r==1 || r+1==n)return n;
+        
+        return Combination(n-1,r)+Combination(n-1,r-1);
+    }
+    vector<vector<int>> generate(int numRows) 
+    {
+        memset(dp, -1, sizeof(dp));
+        numRows--;
+        vector<vector<int>> ans;
+        for(int n=0;n<=numRows;n++)
+        {
+            vector<int>temp;
+            for(int r=0;r<=n;r++)
+            {
+                int temp_var = Combination(n,r);
+                temp.push_back(temp_var);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
