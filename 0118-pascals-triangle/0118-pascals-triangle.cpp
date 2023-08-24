8cc73384758023a26be24995c79c5b64c53aa9cf
Jai Anand <90410312+j-a-i-a-n-a-n-d@users.noreply.github.com>
2023-08-24 18:02:19 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (16.84%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
-    int dp[31][31];
-    
-    int Combination(int n,int r)
+    int nCr(int n, int r)
     {
-        if (r==0 || n==r)return 1;
-        if (r==1 || r+1==n)return n;
-        
-        if(dp[n][r]!=-1)return dp[n][r];
-        
-        return dp[n][r]=Combination(n-1,r)+Combination(n-1,r-1);
+        long long res = 1;
+        for (int i = 0; i < r; i++)
+        {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+    return (int)(res);
     }
     vector<vector<int>> generate(int numRows)
     {
-        memset(dp, -1, sizeof(dp));
-        numRows--;
-        vector<vector<int>> ans;
-        for(int n=0;n<=numRows;n++)
-        {
-            vector<int>temp;
-            for(int r=0;r<=n;r++)
-            {
-                int temp_var = Combination(n,r);
-                temp.push_back(temp_var);
-            }
-            ans.push_back(temp);
-        }
-        return ans;
+         // memset(dp, -1, sizeof(dp));
+         numRows--;
+         vector<vector<int>> ans;
+         for(int n=0;n<=numRows;n++)
+         {
+             vector<int>temp;
+             for(int r=0;r<=n;r++)
+             {
+                 int temp_var = nCr(n,r);
+                 temp.push_back(temp_var);
+             }
+             ans.push_back(temp);
+         }
+         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int dp[31][31];
+    
+//     int Combination(int n,int r)
+//     {
+//         if (r==0 || n==r)return 1;
+//         if (r==1 || r+1==n)return n;
+        
+//         if(dp[n][r]!=-1)return dp[n][r];
+
+//         return dp[n][r]=Combination(n-1,r)+Combination(n-1,r-1);
+//     }
+//     vector<vector<int>> generate(int numRows)
+//     {
+//         memset(dp, -1, sizeof(dp));
+//         numRows--;
+//         vector<vector<int>> ans;
+//         for(int n=0;n<=numRows;n++)
+//         {
+//             vector<int>temp;
+//             for(int r=0;r<=n;r++)
+//             {
+//                 int temp_var = Combination(n,r);
+//                 temp.push_back(temp_var);
+//             }
+//             ans.push_back(temp);
+//         }
+//         return ans;
+//     }
+// };
 
 /*
 class Solution {
